63e07fb70a13b1da8a5ad803400163444505f908
YuangLiu <yuangliu@dhcp-wifi-8021x-155-41-81-16.bu.edu>
2019-09-28 13:55:23 -0400

    ldshjfauaewuhgaea

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,6 +1,8 @@
 public class BlackJack {
     public static void main(String[] args) {
-        BlackJackTable t = new BlackJackTable(Config.PLAYERNUM);
+        System.out.print("How many players in the game? ");
+        int a = Utils.getNumberFromPlayer();
+        BlackJackTable t = new BlackJackTable(a);
         t.playGame();
     }
 }
